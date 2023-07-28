f0518a6904b7d0f591a7edad5da7686d0597bc5d
Diego <diegofabrega18@gmail.com>
2023-07-28 15:50:27 -0400

    ADD: Modules (OS) and notations

---
+++ index.js
@@ -0,0 +1,9 @@
+//variable global en toda la app, todo lo global sale de aqui (console,promise,fetch,etc)
+globalThis.console('');
+
+console.log('testing node');
+
+//Patron de diseño modulo : separar el codigo en diferentes ficheros (import/export)
+export const sum = (num1,num2) => (num1+num2);
+
+// commonJs : require    -   esm6 modules : import export 
