b9462ea04125846202617a059d1becef9bf7c6cb
Diego <diegofabrega18@gmail.com>
2023-07-28 15:52:36 -0400

    UPDATE: Fixed os.map bad syntax

--- native_modules/os.js
+++ native_modules/os.js
@@ -2,7 +2,7 @@ const os = require('node:os');
 
 console.log ('Informacion del sistema: ');
 console.log ('_______________________________________ ');
-os.cpus.map(cpu=>console.log(cpu));
+os.cpus().map(cpu=>console.log(cpu));
 console.log ('S.O :'+os.platform());
 // Etc , con cada info que requiramos del S.O
 
