303fd7863fe06cda28a0a57a58c6bb1c2ba164f5
Diego <diegofabrega18@gmail.com>
2023-07-28 23:12:16 -0400

    ADD: process global variable examples

---
+++ process.js
@@ -0,0 +1,18 @@
+// Objeto process : global que proporciona info y control sobre el proceso actual de ejecucion
+// Propiedades y metodos para interactuar con el entorno e info relacionada con el proceso actual
+
+// Mostrar argumentos de entrada
+console.log(process.argv);
+
+
+// Escuchar eventos del proceso
+process.on('exit',()=>console.log('sali'));
+
+
+// current working directory
+console.log(process.cwd());
+
+
+//Controlar el proceso y su salida
+process.exit(0);
+
